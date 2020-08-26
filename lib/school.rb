416103232494fc42164c50103711994a990b46a7
“saschakala” <mxsaschakala@gmail.com>
2020-08-26 11:54:56 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+require 'pry'
+
+class School
+attr_accessor :grade, :roster, :name
+attr_reader :name
+
+def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+end
+
+def add_student (name, grade)
+    if !roster.key?(grade)
+    roster[grade] = []
+    end
+    roster[grade] << "#{name}"
+end
+
+def grade (grade)
+    roster[grade]
+end
+
+def sort 
+    roster_sorted = {}
+    roster.each do |grade, name|
+        roster_sorted[grade] = name.sort
+    end
+    roster_sorted
+end
+
+
+
+end
