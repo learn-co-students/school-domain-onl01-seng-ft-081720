49c9920bf2a623779664186462ae8eb3849e7198
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-08-27 00:43:58 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+require 'pry'
+
+class School
+    def initialize(roster)
+    @roster={}
+    end
+
+    attr_reader :roster
+
+    def add_student(name, grade)
+
+        @roster[grade.to_i]=[]
+        @roster[grade] << name
+binding.pry
+    end
+    
+end
